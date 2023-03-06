19848214d74d018ebc9eeecf91afe1239e2c8f1b
shinohara-san <shinohara.yuki.2250@moneyforward.co.jp>
2023-03-06 09:46:56 +0900

    Initial Commit

---
+++ tca_omikuji/tca_omikujiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tca_omikujiApp.swift
+//  tca_omikuji
+//
+//  Created by shinohara.yuki.2250 on 2023/03/06.
+//
+
+import SwiftUI
+
+@main
+struct tca_omikujiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
